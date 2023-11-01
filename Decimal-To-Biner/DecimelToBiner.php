8a74fc6e5aa06dcb792ecbc83bf19b89deb29171
Tria Yunita <triaynta@gmail.com>
2023-11-01 10:14:43 +0700

    update

---
+++ Decimal-To-Biner/DecimelToBiner.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konversi Desimal Ke Diner</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&family=Signika+Negative:wght@600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="box">
+        <header>
+            <label for="">Konversi Desimal Ke Diner - Tria</label>
+        </header>
+        <main>
+            <form action="">
+                <div class="row">
+                    <div class="col-md-8">
+                        <label for="" class="form-label">Angka Desimal</label>
+                        <input type="text" class="form-control" id="" placeholder="Masukkan Angka Desimal">
+                    </div>
+                    <br>
+                    <div class="col-md-8">
+                        <label for="" class="form-label">Angka Biner</label>
+                        <input type="text" class="form-control" id="" placeholder="Hasil">
+                    </div><br>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-hitung">Hitung</button>
+                </div>
+            </form>
+        </main>
+    </div>
+</body>
+
+</html>
