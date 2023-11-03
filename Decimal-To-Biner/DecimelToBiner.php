b8890e9de720fdb7b23e992c4dfb5f6f48eeec34
Tria Yunita <triaynta@gmail.com>
2023-11-03 18:58:04 +0700

    third

--- Decimal-To-Biner/DecimelToBiner.php
+++ Decimal-To-Biner/DecimelToBiner.php
@@ -1,32 +1,30 @@
 <!DOCTYPE html>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Konversi Desimal Ke Diner</title>
+    <title>Konversi Desimal Ke Biner</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&family=Signika+Negative:wght@600&display=swap" 
-    rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
-    crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&family=Signika+Negative:wght@600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div id="box">
         <header>
-            <label for="">Konversi Desimal Ke Diner</label>
+            <label for="">Konversi Desimal Ke Biner</label>
         </header>
         <main>
             <form method="post" action="">
                 <div class="row">
                     <div class="col-md-8">
                         <label for="" class="form-label">Angka Desimal</label>
-                        <input type="text" class="form-control" name="decimal" 
-                        placeholder="Masukkan Angka Desimal">
+                        <input type="text" class="form-control" name="decimal" placeholder="Masukkan Angka Desimal">
                         <button type="submit">Hitung</button>
                     </div>
                 </div>
